0a50386bddbe88403b68c1391736b5acd75d2fe7
James Plasencia <jamesplasencia@gmail.com>
2022-07-24 14:44:38 -0700

    created connection file and set up sequelize

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+// const sequelize = require("./config/connection");
 console.log("connected!");
+
+// turn on connection to db and server
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "test",
+// });
 
 // menu to view departments, roles, employees
 const promptDB = () => {
@@ -9,7 +17,7 @@ const promptDB = () => {
     .prompt([
       {
         type: "list",
-        names: "databases",
+        name: "databases",
         message: "What would you like to view?",
         choices: [
           "Departments",
@@ -23,24 +31,49 @@ const promptDB = () => {
     .then((choices) => {
       switch (choices.databases) {
         case "Departments":
-          promptDepartment();
+          tableDepartment();
           break;
         case "Roles":
-          promptRoles();
+          tableRoles();
           break;
         case "Employees":
-          promptEmployees();
+          tableEmployees();
           break;
         case "Add Department, Role, or Employee":
           promptAdd();
           break;
         case "Update Employee Information":
-          promtUpdate();
+          promptUpdate();
           break;
       }
     });
 };
 // add department, add role, add employee, update employee role
+const promptAdd = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "add",
+        message: "What would you like to add?",
+        choices: ["Departments", "Roles", "Employees"],
+      },
+    ])
+    .then((choices) => {
+      switch (choices.add) {
+        case "Departments":
+          addDepartment();
+          break;
+        case "Roles":
+          addRoles();
+          break;
+        case "Employees":
+          addEmployees();
+          break;
+      }
+    });
+};
+
 // view all dept->dept name and ids
 // view all roles->job titles, role id, dept for role, salary
 // view all emp->ids, first name, last name, title, dept, manager
@@ -49,3 +82,4 @@ const promptDB = () => {
 // add role->name, salary, dept-create
 // add emp->name, role, manager-create
 // update emp->select and update new role-update
+promptDB();
